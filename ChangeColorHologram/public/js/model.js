5e19c46bd360ea0131fa252aa63ca048b63d35ba
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-04-11 12:05:59 +0200

    feat: add synchronization for button clicked

--- ChangeColorHologram/public/js/model.js
+++ ChangeColorHologram/public/js/model.js
@@ -8,12 +8,49 @@ class RootModel extends Croquet.Model {
         this.hologramChildren = [];
 
         this.subscribe("hologram", "created", this.addHologram);
+        this.subscribe("colorButton", "clicked", this.colorButtonClicked);
         console.log("MODEL initialized");
     }
 
+    colorButtonClicked(data){
+        console.log("MODEL received: color button clicked " + data.name);
+        const color = this.#computeColor(data.name);
+        this.hologramChildren.forEach(hologram => hologram.material.diffuseColor = color);
+    }
+
     addHologram(data){
-        console.log("MODEL received: hologram created " + data);
-        this.hologramChildren.push(this.scene.meshes.find(m => m.name === data.name));
+         console.log("MODEL received: hologram created " + data.name);
+         console.log(this.scene.meshes.map(m => m.name));
+         console.log(this.hologramChildren);
+         const hologram = this.scene.meshes.find(m => m.name === data.name);
+         if (!(typeof hologram === "undefined")) {
+             this.hologramChildren.push(hologram);
+         }
+    }
+
+    #computeColor(colorName){
+        switch (colorName) {
+            case "Blue":
+                return BABYLON.Color3.Blue();
+                break;
+            case "Red":
+                return BABYLON.Color3.Red();
+                break;
+            case "Green":
+                return BABYLON.Color3.Green();
+                break;
+            case "Purple":
+                return BABYLON.Color3.Purple();
+                break;
+            case "Yellow":
+                return BABYLON.Color3.Yellow();
+                break;
+            case "Teal":
+                return BABYLON.Color3.Teal();
+                break;
+            default:
+                return BABYLON.Color3.White();
+        }
     }
 
 }
--- ChangeColorHologram/public/js/view.js
+++ ChangeColorHologram/public/js/view.js
@@ -14,7 +14,7 @@ class RootView extends Croquet.View {
 
         const alpha = -Math.PI / 2;//Math.PI/4;
         const beta = Math.PI / 2;
-        const radius = 2;
+        const radius = 0;
         const target = new BABYLON.Vector3(0, 0, 0);
 
         const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, this.model.scene);//camera that can be rotated around a target
@@ -23,23 +23,59 @@ class RootView extends Croquet.View {
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
         light.intensity = 1;
 
-        this.#addHolographicElement();
+        this.GUIManager = new BABYLON.GUI.GUI3DManager(this.model.scene);
 
+
+        this.#addHolographicElement();
+        this.#addNearMenu();
+
+    }
+
+    notifyButtonClicked(name){
+        console.log("VIEW publish: colorButton clicked " + name);
+        this.publish("colorButton", "clicked", {name: name});
     }
 
     #addHolographicElement() {
-         const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 0.2, segments: 32}, this.model.scene);
+        const sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 0.2, segments: 32}, this.model.scene);
         sphere.position =  new BABYLON.Vector3(0, 1.3, 1);
 
         const material = new BABYLON.StandardMaterial("material", this.model.scene);
-        material.diffuseColor = BABYLON.Color3.Random();
+        material.diffuseColor = BABYLON.Color3.White();
 
         sphere.material = material;
-        console.log("VIEW: " + sphere.name);
+        console.log("VIEW: publish hologram created" + sphere.name);
         this.publish("hologram", "created", {name: sphere.name});
     }
 
-    #add
+    #addNearMenu(){
+        this.GUIManager.useRealisticScaling = true;
+
+        const buttonParams = [
+            { name: "Blue", color: BABYLON.Color3.Blue() },
+            { name: "Red", color: BABYLON.Color3.Red() },
+            { name: "Green", color: BABYLON.Color3.Green() },
+            { name: "Purple", color: BABYLON.Color3.Purple() },
+            { name: "Yellow", color: BABYLON.Color3.Yellow() },
+            { name: "Teal", color: BABYLON.Color3.Teal() },
+        ]
+
+        const nearMenu = new BABYLON.GUI.NearMenu("NearMenu");
+        nearMenu.rows = 3;
+        this.GUIManager.addControl(nearMenu);
+        nearMenu.isPinned = true;
+        nearMenu.position = new BABYLON.Vector3(-0.2, 1.3, 1);
+
+        buttonParams.forEach(input => {
+            const button = new BABYLON.GUI.TouchHolographicButton();
+            button.text = input.name;
+            button.onPointerDownObservable.add(() => {
+                this.notifyButtonClicked(input.name);
+            });
+
+            nearMenu.addButton(button);
+        })
+    }
 
     async #createWebXRExperience() {
         const supported = await BABYLON.WebXRSessionManager.IsSessionSupportedAsync('immersive-ar')
