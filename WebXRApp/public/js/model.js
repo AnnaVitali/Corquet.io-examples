902b10973083f3fd55c115478afcc58055816dbe
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-04-15 12:36:26 +0200

    feat: create webxr app that enable moving and changing hologram color

--- WebXRApp/public/js/model.js
+++ WebXRApp/public/js/model.js
@@ -1,24 +1,104 @@
 const canvas = document.getElementById("renderCanvas");
-const engine = new BABYLON.Engine(canvas, true);
-const scene = new BABYLON.Scene(engine);
 
 class RootModel extends Croquet.Model {
 
     init() {
-        scene.clearColor = new BABYLON.Color3.Black;
+        this.linkedViews = [];
 
+        this.subscribe(this.sessionId, "view-join", this.viewJoin);
+        this.subscribe(this.sessionId, "view-exit", this.viewDrop)
+        this.subscribe("colorButton", "clicked", this.changeHologramColor);
+        this.subscribe("controlButton", "clicked", this.manageUserHologramControl);
+        this.subscribe("hologram", "positionChanged", this.updatePosition);
+
+        this.#initializeScene();
+        this.#activateRenderLoop();
+
+    }
+
+    updatePosition(data){
+        const position = new BABYLON.Vector3(data.position_x, data.position_y, data.position_z);
+        const rotation = new BABYLON.Quaternion(data.rotation_x, data.rotation_y, data.rotation_z, data.rotation_w);
+
+        this.sphere.position = position;
+        this.sphere.rotationQuaternion = rotation;
+    }
+
+    viewJoin(viewId){
+        console.log("MODEL: received view join");
+        this.linkedViews.push(viewId);
+    }
+
+    viewDrop(viewId){
+        console.log("MODEL: received view left");
+        this.linkedViews.splice(this.linkedViews.indexOf(viewId),1);
+    }
+
+    manageUserHologramControl(data){
+        console.log("MODEL: received manage user hologram control");
+        console.log("id received: " + data.view);
+        console.log(this.linkedViews);
+        this.linkedViews.filter(v => data.view !== v).forEach(v => this.publish(v, "hideManipulatorMenu"));
+    }
+
+    changeHologramColor(data){
+        console.log("MODEL: receive color button clicked");
+        this.sphere.material.diffuseColor = this.#computeColor(data.color);
+    }
+
+    #computeColor(colorName){
+        switch (colorName) {
+            case "Blue":
+                return BABYLON.Color3.Blue();
+                break;
+            case "Red":
+                return BABYLON.Color3.Red();
+                break;
+            case "Green":
+                return BABYLON.Color3.Green();
+                break;
+            case "Purple":
+                return BABYLON.Color3.Purple();
+                break;
+            case "Yellow":
+                return BABYLON.Color3.Yellow();
+                break;
+            case "Teal":
+                return BABYLON.Color3.Teal();
+                break;
+            default:
+                return BABYLON.Color3.White();
+        }
+    }
+
+    #initializeScene(){
+        this.engine = new BABYLON.Engine(canvas, true);
+        this.scene = new BABYLON.Scene(this.engine);
+        this.scene.clearColor = new BABYLON.Color3.Black;
+        /*
         const alpha = -Math.PI / 2;//Math.PI/4;
         const beta = Math.PI / 2;
         const radius = 2;
-        const target = new BABYLON.Vector3(0, 0, 0);
+        const target = new BABYLON.Vector3(0, 0, 0);*/
 
-        const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, scene);//camera that can be rotated around a target
+        //const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, this.scene);//camera that can be rotated around a target
+        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 1.7, -0.3), this.scene);
+        camera.minZ = 0.01;
         camera.attachControl(canvas, true);
 
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
         light.intensity = 1;
-        this.#activateRenderLoop();
 
+        this.GUIManager = new BABYLON.GUI.GUI3DManager(this.scene);
+        this.GUIManager.useRealisticScaling = true;
+
+        this.sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 0.2, segments: 32}, this.scene);
+        this.sphere.position = new BABYLON.Vector3(0, 1.2, 0.5); //new BABYLON.Vector3(0, 1.3, 1);
+
+        const material = new BABYLON.StandardMaterial("material", this.scene);
+        material.diffuseColor = BABYLON.Color3.White();
+
+        this.sphere.material = material;
     }
 
     async #createWebXRExperience() {
@@ -26,7 +106,7 @@ class RootModel extends Croquet.Model {
 
         if (supported) {
             console.log("IMMERSIVE AR SUPPORTED");
-            const xrHelper = await scene.createDefaultXRExperienceAsync({
+            const xrHelper = await this.scene.createDefaultXRExperienceAsync({
                 uiOptions: {
                     sessionMode: 'immersive-ar',
                     referenceSpaceType: "local-floor"
@@ -34,7 +114,7 @@ class RootModel extends Croquet.Model {
             });
         } else {
             console.log("IMMERSIVE VR SUPPORTED")
-            const xrHelper = await scene.createDefaultXRExperienceAsync({
+            const xrHelper = await this.scene.createDefaultXRExperienceAsync({
                 uiOptions: {
                     sessionMode: 'immersive-vr',
                 }
@@ -43,16 +123,17 @@ class RootModel extends Croquet.Model {
 
         try {
             xrHelper.baseExperience.featuresManager.enableFeature(BABYLON.WebXRFeatureName.HAND_TRACKING, "latest", { xrInput: xr.input });
+            xrHelper.baseExperience.camera.position = new BABYLON.Vector3(0, 0, -0.3);
         } catch (err) {
             console.log("Articulated hand tracking not supported in this browser.");
         }
 
-        return scene;
+        return this.scene;
     }
 
     #activateRenderLoop() {
         this.#createWebXRExperience().then(sceneToRender => {
-            engine.runRenderLoop(() => sceneToRender.render());
+            this.engine.runRenderLoop(() => sceneToRender.render());
         });
     }
 
--- WebXRApp/public/js/view.js
+++ WebXRApp/public/js/view.js
@@ -3,7 +3,152 @@ class RootView extends Croquet.View {
     constructor(model) {
         super(model);
         this.model = model;
+
+       // this.subscribe("controlButton", "clicked", this.manageUserHologramControls);
+        this.subscribe(this.viewId, "hideManipulatorMenu", this.hideManipulatorMenu);
+
+        console.log("VIEW subscribed ");
+        this.#addChangeColorMenu();
+        this.#addManipulatorMenu();
+    }
+
+    hideManipulatorMenu(){
+        console.log("VIEW: received hide manipulator menu");
+        this.manipulatorNearMenu.dispose();
+    }
+
+    notifyColorButtonClicked(colorName){
+        console.log("VIEW: color button clicked");
+        this.publish("colorButton", "clicked", {color: colorName});
+    }
+
+    notifyUserStartManipulating(){
+        console.log("VIEW: user start manipulating");
+        this.publish("controlButton", "clicked", {view: this.viewId});
+    }
+
+    notifyHologramPositionChanged(position, rotation){
+        console.log("VIEW: hologram position change");
+        this.publish("hologram", "positionChanged",
+            {
+                position_x: position.x,
+                position_y: position.y,
+                position_z: position.z,
+                rotation_x: rotation.x,
+                rotation_y: rotation.y,
+                rotation_z: rotation.z,
+                rotation_w: rotation.w
+            });
+    }
+
+    #addChangeColorMenu(){
+        const buttonParams = [
+            { name: "Blue", color: BABYLON.Color3.Blue() },
+            { name: "Red", color: BABYLON.Color3.Red() },
+            { name: "Green", color: BABYLON.Color3.Green() },
+            { name: "Purple", color: BABYLON.Color3.Purple() },
+            { name: "Yellow", color: BABYLON.Color3.Yellow() },
+            { name: "Teal", color: BABYLON.Color3.Teal() },
+        ]
+
+        const nearMenu = new BABYLON.GUI.NearMenu("NearMenu");
+        nearMenu.rows = 3;
+        this.model.GUIManager.addControl(nearMenu);
+        nearMenu.isPinned = true;
+        nearMenu.position = new BABYLON.Vector3(-0.2, 1.2, 0.5);
+
+        buttonParams.forEach(input => {
+            const button = new BABYLON.GUI.TouchHolographicButton();
+            button.text = input.name;
+            button.onPointerDownObservable.add(() => {
+                this.notifyColorButtonClicked(input.name);
+            });
+
+            nearMenu.addButton(button);
+        })
+    }
+
+    #addManipulatorMenu(){
+        this.manipulatorNearMenu = new BABYLON.GUI.NearMenu("NearMenu");
+        this.manipulatorNearMenu.rows = 1;
+        this.model.GUIManager.addControl( this.manipulatorNearMenu);
+        this.manipulatorNearMenu.isPinned = true;
+        this.manipulatorNearMenu.position = new BABYLON.Vector3(+0.2, 1.2, 0.5);
+
+        this.controlButton = new BABYLON.GUI.TouchHolographicButton();
+        this.#setDefaultControlButtonBehavior()
+        this.manipulatorNearMenu.addButton(this.controlButton);
+    }
+
+    #setDefaultControlButtonBehavior() {
+        this.controlButton.text = "Start manipulating";
+        this.controlButton.imageUrl = "../img/iconAdjust.png"
+        this.controlButton.onPointerDownObservable.clear();
+        this.controlButton.onPointerDownObservable.add(() => {
+            this.#addHologramManipulator();
+            this.notifyUserStartManipulating();
+        });
+    }
+
+    #addHologramManipulator(){
+        //create bounding box and object controls
+        this.viewSphere = BABYLON.MeshBuilder.CreateSphere("viewSphere", {diameter: 0.2, segments: 32}, this.model.scene);
+        this.viewSphere.position = new BABYLON.Vector3(this.model.sphere.absolutePosition.x, this.model.sphere.absolutePosition.y, this.model.sphere.absolutePosition.z);
+        this.viewSphere.rotationQuaternion = new BABYLON.Quaternion(this.model.sphere.absoluteRotationQuaternion.x, this.model.sphere.absoluteRotationQuaternion.y, this.model.sphere.absoluteRotationQuaternion.z, this.model.sphere.absoluteRotationQuaternion.w);
+        this.viewSphere.isVisible = false;
+
+        this.boundingBox = BABYLON.BoundingBoxGizmo.MakeNotPickableAndWrapInBoundingBox(this.viewSphere);
+        const utilLayer = new BABYLON.UtilityLayerRenderer(this.model.scene);
+        utilLayer.utilityLayerScene.autoClearDepthAndStencil = false;
+        this.gizmo = new BABYLON.BoundingBoxGizmo(BABYLON.Color3.FromHexString("#0984e3"), utilLayer)
+        this.gizmo.rotationSphereSize = 0.03;
+        this.gizmo.scaleBoxSize = 0.03;
+        this.gizmo.attachedMesh = this.boundingBox;
+
+        // Create behaviors to drag and scale with pointers in VR
+        const sixDofDragBehavior = new BABYLON.SixDofDragBehavior();
+        sixDofDragBehavior.dragDeltaRatio = 1;
+        sixDofDragBehavior.zDragFactor = 1;
+
+        sixDofDragBehavior.onPositionChangedObservable.add(() => {
+            this.notifyHologramPositionChanged(this.viewSphere.absolutePosition, gizmo.onScaleBoxDragObservable.add(() => {
+                console.log("scaleDrag");
+            });
+            gizmo.onScaleBoxDragEndObservable.add(() => {
+                console.log("scaleEnd");
+            });
+            gizmo.onRotationSphereDragObservable.add(() => {
+                console.log("rotDrag");
+            });
+            gizmo.onRotationSphereDragEndObservable.add(() => {
+                console.log("rotEnd");
+            }););
+        });
+        this.boundingBox.addBehavior(sixDofDragBehavior);
+
+        this.gizmo.onScaleBoxDragObservable.add(() => {
+            console.log("scaleDrag");
+        });
+        this.gizmo.onScaleBoxDragEndObservable.add(() => {
+            console.log("scaleEnd");
+        });
+        this.gizmo.onRotationSphereDragObservable.add(() => {
+            console.log("rotDrag");
+        });
+
+        /*
+        var multiPointerScaleBehavior = new BABYLON.MultiPointerScaleBehavior();
+        this.boundingBox.addBehavior(multiPointerScaleBehavior);
+        */
+
+        this.controlButton.text = "Stop manipulating";
+        this.controlButton.onPointerDownObservable.clear();
+        this.controlButton.onPointerDownObservable.add(() => {
+            //this.notifyCurrentUserReleaseControl();
+        });
     }
 }
+
+
 
 export { RootView };
